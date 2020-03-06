7b92679130c4d7ac3dc16ca7f3508859e496e3f7
Markus Unger <schaulustig23@gmail.com>
2020-03-06 17:15:43 +0100

    basic functionality for the one-button page

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const ehb = require('express-handlebars');
+const path = require('path');
+
+const frontendRouter = require('./routes/frontendRouter');
+// const apiRouter = require('./routes/apiRouter');
+
+const app = express();
+
+app.disable('x-powered-by');
+
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine('hbs', ehb({
+  layoutsDir: './views/layouts',
+  defaultLayout: 'main',
+  extname: '.hbs',
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/api', express.json());
+// app.use('/api', apiRouter);
+
+app.use('/', frontendRouter);
+
+module.exports = app;
